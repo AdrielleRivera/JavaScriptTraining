f227bdb938fe1a0bfe7b249d76281a8e8cd4dd1a
Adrielle Rivera <mr.adriellerivera@gmail.com>
2024-09-16 16:12:39 +0800

    Update script.js

--- script.js
+++ script.js
@@ -3,17 +3,19 @@ const count = 8;
 const rows = [];
 
 function padRow(name) {
+  const test = "Testing";  
   return character + name;
 }
 
 const call = padRow("CamperChan");
 console.log(call);
 
+
 for (let i = 0; i < count; i = i + 1) {
-  rows.push(character.repeat(i + 1));
+  rows.push(character.repeat(i + 1))
 }
 
-let result = "";
+let result = ""
 
 for (const row of rows) {
   result = result + "\n" + row;
